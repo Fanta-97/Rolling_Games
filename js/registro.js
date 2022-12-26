38eead79672da3ef56dfdc8e351339193075eb1c
Joaquin <joaquinutnfrt@gmail.com>
2022-12-26 13:11:30 -0300

    terminando registro

--- js/registro.js
+++ js/registro.js
@@ -2,6 +2,14 @@ let regEx_pass = /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[a-zA-Z]).{8,}$/gm
 let regEx_email = /[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?/g 
 const EDAD_MINIMA_MILISEGUNDOS = 409968000000 //13 años
 
+const form = document.getElementById('form')
+form.addEventListener('submit', evt => {
+  console.log(evt)
+})
+
+
+
+
 const fechaValida = (fecha) => {
   const fechaElegida = new Date(fecha).getTime()
   const fechaActual = Date.now()
@@ -25,14 +33,24 @@ const mostrarMensajeEstado = (titulo, mensaje) => {
   const $p = document.getElementById('modal-body-p').innerText = mensaje
 }
 
-const iniciarRegistro = () => {
+const capturarDatos = () => {
   let fecha = document.getElementById('date').value
   let nombre = document.getElementById('nombre').value
   let apellido = document.getElementById('apellido').value
   let email = document.getElementById('email').value
   let password = document.getElementById('password').value
+  return { email, password, fecha, nombre, apellido, rol: 'usuario', estado: "1"}
+}
 
-  if(!validarDatos(email, password, fecha))
+const btnInicioSesion = document.getElementById('btnInicioSesion').addEventListener("click", e => {
+  e.preventDefault()
+  iniciarRegistro()
+})
+
+
+const iniciarRegistro = () => {
+  const unUsuario = capturarDatos()
+  if(!validarDatos(unUsuario.email, unUsuario.password, unUsuario.fecha))
     return mostrarMensajeEstado('ERROR DE CARGA', 'Los datos proporcionados no son válidos')
   else {
     fetch('http://localhost:3000/users')
@@ -40,21 +58,17 @@ const iniciarRegistro = () => {
       .then( users => {
         const existe = users.filter( user => {
           const {emailU, passwordU} = user
-          return email === emailU && password === passwordU
+          return unUsuario.email === emailU && unUsuario.password === passwordU
         })
         if(existe.length == 1) return mostrarMensajeEstado("USUARIO EXISTENTE", `el usuario ${email} se encuentra registrado`)
       } )  
     
-    const nuevoUsuario = { email, password, fecha, nombre, apellido, rol: 'usuario', estado: "1", }
-
     fetch('http://localhost:3000/users', {
-    method: 'POST',
-    body: JSON.stringify(nuevoUsuario),
-    headers: {
-      'Content-type': 'application/json; charset=UTF-8',
-    },
+      method: 'POST',
+      body: JSON.stringify(unUsuario),
+      headers: { 'Content-type': 'application/json; charset=UTF-8', },
     })
-  .then((response) => response.json())
-  .then((json) => console.log(json));
+      .then((response) => response.json())
+      .then( json => mostrarMensajeEstado("USUARIO REGISTRADO", `El usuario ${email} fue registrado exitosamente`))
   }
 }
